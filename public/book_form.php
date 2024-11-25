8304479cfa23099163a34011588d0306dd85d0dd
student-mousam <147028324+student-mousam@users.noreply.github.com>
2024-11-25 20:50:01 +0530

    Add files via upload

---
+++ public/book_form.php
@@ -0,0 +1,23 @@
+<?php
+$connection = mysqli_connect('localhost', 'root', '', 'book_db');
+
+if(isset($_POST['send'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $location = $_POST['location'];
+    $guests = $_POST['guests'];
+    $arrivals = $_POST['arrivals'];
+    $leaving = $_POST['leaving'];
+
+    $request = "INSERT INTO book_form (name, email, phone, address, location, guests, arrivals, leaving) 
+                VALUES ('$name', '$email', '$phone', '$address', '$location', '$guests', '$arrivals', '$leaving')";
+
+    mysqli_query($connection, $request);
+
+    header('Location: book.php');
+} else {
+    echo 'Something went wrong, please try again.';
+}
+?>
